41890c8fd6e1c2d380e3400e6b5193fb5f493264
Matheus Sampaio <matheus.sampaio@embedded.ufcg.edu.br>
2015-02-25 17:35:01 -0300

    Adicionando tipos de perigo diferentes

--- WumpusIA/src/main/java/org/wumpusia/core/Main.java
+++ WumpusIA/src/main/java/org/wumpusia/core/Main.java
@@ -43,47 +43,49 @@ public class Main {
 
             final String nextMove = findNextMove(graph);
 
-            //            System.out.println(String.format("up: %s - down: %s - left: %s - right: %s",
-            //                    mCurrentNode.getAttribute("up"), mCurrentNode.getAttribute("down"),
-            //                    mCurrentNode.getAttribute("left"), mCurrentNode.getAttribute("right")));
+            System.out.println(nextMove);
 
-            //            System.out.println(wumpus.toString());
+            //            execMove(wumpus, nextMove);
 
-            //            final String in = sc.nextLine();
+            System.out.println(wumpus.toString());
 
-            //            if (in.equalsIgnoreCase("w")) {
-            //                wumpus.moveUp();
-            //            } else if (in.equalsIgnoreCase("s")) {
-            //                wumpus.moveDown();
-            //            } else if (in.equalsIgnoreCase("a")) {
-            //                wumpus.moveLeft();
-            //            } else if (in.equalsIgnoreCase("d")) {
-            //                wumpus.moveRight();
-            //            }
+            final String in = sc.nextLine();
 
-            System.out.println("Moving to: " + nextMove);
-
-            if (nextMove.equalsIgnoreCase("up")) {
+            if (in.equalsIgnoreCase("w")) {
                 wumpus.moveUp();
-            } else if (nextMove.equalsIgnoreCase("down")) {
+            } else if (in.equalsIgnoreCase("s")) {
                 wumpus.moveDown();
-            } else if (nextMove.equalsIgnoreCase("left")) {
+            } else if (in.equalsIgnoreCase("a")) {
                 wumpus.moveLeft();
-            } else if (nextMove.equalsIgnoreCase("right")) {
+            } else if (in.equalsIgnoreCase("d")) {
                 wumpus.moveRight();
             }
 
-            try {
-                Thread.sleep(2000);
-            } catch (final InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
         }
 
         sc.nextLine();
         display.close();
 
+    }
+
+    private static void execMove(final Wumpus wumpus, final String nextMove) {
+        System.out.println("Moving to: " + nextMove);
+
+        if (nextMove.equalsIgnoreCase("up")) {
+            wumpus.moveUp();
+        } else if (nextMove.equalsIgnoreCase("down")) {
+            wumpus.moveDown();
+        } else if (nextMove.equalsIgnoreCase("left")) {
+            wumpus.moveLeft();
+        } else if (nextMove.equalsIgnoreCase("right")) {
+            wumpus.moveRight();
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (final InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private static boolean refreshNodes(final Graph graph, final Wumpus wumpus) {
@@ -110,30 +112,36 @@ public class Main {
 
     private static void refreshDanger(final Graph graph, final Wumpus wumpus) {
         if (wumpus.getCurrentCell().getHazard().equals(Hazard.NONE)) {
-            graph.getNode(wumpus.getCurrentCell().getId()).setAttribute("danger", 0);
+            graph.getNode(wumpus.getCurrentCell().getId()).setAttribute("danger-flap", 0);
+            graph.getNode(wumpus.getCurrentCell().getId()).setAttribute("danger-breeze", 0);
+            graph.getNode(wumpus.getCurrentCell().getId()).setAttribute("danger-wumpus", 0);
         }
 
         if (wumpus.getCurrentCell().isFeelBreeze()) {
-            addVizinhosDanger(graph, wumpus, 25);
+            addVizinhosDanger(graph, wumpus, "breeze", 25);
+        } else {
+            setVizinhosSafe(graph, wumpus, "danger-breeze");
         }
 
         if (wumpus.getCurrentCell().isHearFlapping()) {
-            addVizinhosDanger(graph, wumpus, 25);
+            addVizinhosDanger(graph, wumpus, "flap", 25);
+        } else {
+            setVizinhosSafe(graph, wumpus, "danger-flap");
         }
 
         if (wumpus.getCurrentCell().isSmellWumpus()) {
-            addVizinhosDanger(graph, wumpus, 25);
-        }
-
-        if (!wumpus.getCurrentCell().isSmellWumpus() && !wumpus.getCurrentCell().isFeelBreeze()
-                && !wumpus.getCurrentCell().isHearFlapping()) {
-            setVizinhosSafe(graph, wumpus);
+            addVizinhosDanger(graph, wumpus, "wumpus", 25);
+        } else {
+            setVizinhosSafe(graph, wumpus, "danger-wumpus");
         }
     }
 
     private static void refreshNodesLabel(final Graph graph) {
         for (final Node n : graph) {
-            n.setAttribute("ui.label", n.getAttribute("id") + " : " + n.getAttribute("danger"));
+            final String msg = String.format("%s : (%s,%s,%s)", n.getAttribute("id"),
+                    n.getAttribute("danger-wumpus"), n.getAttribute("danger-breeze"),
+                    n.getAttribute("danger-flap"));
+            n.setAttribute("ui.label", msg);
         }
     }
 
@@ -185,7 +193,9 @@ public class Main {
             node = graph.addNode(cell.getId());
 
             node.setAttribute("id", cell.getId());
-            node.setAttribute("danger", 0);
+            node.setAttribute("danger-wumpus", 0);
+            node.setAttribute("danger-breeze", 0);
+            node.setAttribute("danger-flap", 0);
             node.setAttribute("visited", false);
         }
 
@@ -201,11 +211,15 @@ public class Main {
         while (breadthFirstIterator.hasNext()) {
             final Node node = breadthFirstIterator.next();
             final String nodeClass = node.getAttribute("ui.class");
-            final Integer nodeDanger = node.getAttribute("danger");
 
-            if (nodeClass.equals("unvisited") && nodeDanger < minDanger) {
+            final Integer dangerBreeze = node.getAttribute("danger-breeze");
+            final Integer dangerFlap = node.getAttribute("danger-flap");
+            final Integer dangerWumpus = node.getAttribute("danger-wumpus");
+
+            if (nodeClass.equals("unvisited")
+                    && dangerBreeze + dangerFlap + dangerWumpus < minDanger) {
                 nodeResult = node;
-                minDanger = nodeDanger;
+                minDanger = dangerBreeze + dangerFlap + dangerWumpus;
             }
         }
 
@@ -230,60 +244,56 @@ public class Main {
         return move;
     }
 
-    private static void addVizinhosDanger(final Graph graph, final Wumpus wumpus, final int danger) {
+    private static void addVizinhosDanger(final Graph graph, final Wumpus wumpus,
+            final String dangerType, final int danger) {
         final Cell currentCell = wumpus.getCurrentCell();
 
         if (wumpus.hasUpCell(currentCell)) {
-            addDanger(graph, currentCell, wumpus.getUpCell(currentCell), danger);
+            addDanger(graph, currentCell, wumpus.getUpCell(currentCell), dangerType, danger);
         }
 
         if (wumpus.hasDownCell(currentCell)) {
-            addDanger(graph, currentCell, wumpus.getDownCell(currentCell), danger);
+            addDanger(graph, currentCell, wumpus.getDownCell(currentCell), dangerType, danger);
         }
 
         if (wumpus.hasLeftCell(currentCell)) {
-            addDanger(graph, currentCell, wumpus.getLeftCell(currentCell), danger);
+            addDanger(graph, currentCell, wumpus.getLeftCell(currentCell), dangerType, danger);
         }
 
         if (wumpus.hasRightCell(currentCell)) {
-            addDanger(graph, currentCell, wumpus.getRightCell(currentCell), danger);
+            addDanger(graph, currentCell, wumpus.getRightCell(currentCell), dangerType, danger);
         }
 
     }
 
     private static void addDanger(final Graph graph, final Cell currentCell, final Cell otherCell,
-            final Integer danger) {
+            final String dangerType, final Integer danger) {
         final Node n = graph.getNode(otherCell.getId());
 
         if (!n.hasAttribute("safe")) {
-            final Integer oldDanger = n.getAttribute("danger");
-            n.setAttribute("danger", oldDanger + danger);
+            final Integer oldDanger = n.getAttribute("danger-" + dangerType);
+            n.setAttribute("danger-" + dangerType, oldDanger + danger);
         }
     }
 
-    private static void setVizinhosSafe(final Graph graph, final Wumpus wumpus) {
+    private static void setVizinhosSafe(final Graph graph, final Wumpus wumpus,
+            final String dangerType) {
         final Cell currentCell = wumpus.getCurrentCell();
 
         if (wumpus.hasUpCell(currentCell)) {
-            graph.getNode(wumpus.getUpCell(currentCell).getId()).setAttribute("safe", true);
-            graph.getNode(wumpus.getUpCell(currentCell).getId()).setAttribute("danger", 0);
+            graph.getNode(wumpus.getUpCell(currentCell).getId()).setAttribute(dangerType, 0);
         }
 
         if (wumpus.hasDownCell(currentCell)) {
-            graph.getNode(wumpus.getDownCell(currentCell).getId())
-                    .setAttribute("safe", true);
-            graph.getNode(wumpus.getDownCell(currentCell).getId()).setAttribute("danger", 0);
+            graph.getNode(wumpus.getDownCell(currentCell).getId()).setAttribute(dangerType, 0);
         }
 
         if (wumpus.hasLeftCell(currentCell)) {
-            graph.getNode(wumpus.getLeftCell(currentCell).getId())
-                    .setAttribute("safe", true);
-            graph.getNode(wumpus.getLeftCell(currentCell).getId()).setAttribute("danger", 0);
+            graph.getNode(wumpus.getLeftCell(currentCell).getId()).setAttribute(dangerType, 0);
         }
 
         if (wumpus.hasRightCell(currentCell)) {
-            graph.getNode(wumpus.getRightCell(currentCell).getId()).setAttribute("safe", true);
-            graph.getNode(wumpus.getRightCell(currentCell).getId()).setAttribute("danger", 0);
+            graph.getNode(wumpus.getRightCell(currentCell).getId()).setAttribute(dangerType, 0);
         }
 
     }
